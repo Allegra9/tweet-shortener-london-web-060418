9c105c97a308a52076202b40faf5dfda0797fa6d
Allegra <aukseallegra@gmail.com>
2018-06-02 21:33:51 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,13 @@
 def dictionary(tweet)
-  
-  
+  dictionary = {
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
 end 
